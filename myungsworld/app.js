9416c339882150c7adf4d62f8266ff50be2a3a15
myungsworld <myungsworld@MacBook-Pro.local>
2020-06-21 18:56:05 +0900

    deduplication and layout doesnt work

--- myungsworld/app.js
+++ myungsworld/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var app = express();
+var expressLayouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -18,7 +19,10 @@ app.use('/', indexRouter);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// /app.set('layout','layout');
+// app.set("layout extractScripts", true);
 
+app.use(expressLayouts);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
