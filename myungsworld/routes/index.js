eb1f4bc83919fbbf3e6ec59be3325ae67462a184
myungsworld <myungsworld@gmail.com>
2020-06-26 13:32:58 +0900

    updated rendering

--- myungsworld/routes/index.js
+++ myungsworld/routes/index.js
@@ -7,7 +7,7 @@ router.use('/users',usersRouter);
 // ./ same folder path ../ previous folder path
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/myungsworld', function(req, res, next) {
   //res.render('index', { title: 'Express' }) 서버가 클라이언트에게 index.ejs를 렌더링하여 보내겠다는 의미
   res.render('index', { title: 'Myungsworld' });
 });
