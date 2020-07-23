b72bc7ae8c2ef42f4fee1d58d6afb04c42307e96
myungsworld <myungsworld@gmail.com>
2020-07-23 13:39:51 +0900

    orientdb

--- study/database_orientdb.js
+++ study/database_orientdb.js
@@ -9,7 +9,16 @@ var server = OrientDB({
 
 var db = server.use('o2')
 
+/*
 //저 고유번호를 가진 데이터를 가져오면 콜백 함수를 실행
 db.record.get('#35:0').then(function (record) {
-    console.log('Loaded record', record)
+    console.log('Loaded record', record.title)
 })
+*/
+
+//CREATE
+var sql = 'SELECT FROM topic';
+db.query(sql).then((results) => {
+    console.log(results)
+})
+
