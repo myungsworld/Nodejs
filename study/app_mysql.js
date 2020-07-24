be39ea713e142f530fd93a6ac6a1d7aa0a0bbcb2
myungsworld <myungsworld@gmail.com>
2020-07-25 00:51:26 +0900

    CRUD updated

--- study/app_mysql.js
+++ study/app_mysql.js
@@ -22,6 +22,44 @@ app.set('views','./views_mysql');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public_file'));
 
+app.get(['/topic/:id/edit'], (req,res) => {
+    var sql = `SELECT id,title FROM topic`
+    conn.query(sql, (err, topics, fields) => {
+        var id = req.params.id;
+        if(id){
+            var sql ='SELECT * FROM topic WHERE id=?'
+            conn.query(sql,[id], (err, topic, fields) => {
+                if(err){
+                    console.log(err);
+                    res.status(500).send('Internal Server Error')
+                } else {
+                    res.render('edit', {topics:topics, topic:topic[0]})
+                }
+            })
+        } else {
+            console.log('There is no id.');
+            res.status(500).send('Internal Server Error')
+        }
+    })
+})
+
+app.post(['/topic/:id/edit'], (req,res) => {
+    var title = req.body.title
+    var description =req.body.description
+    var author = req.body.author 
+    var id =req.params.id;
+    var sql = `UPDATE topic SET title=?, description=?, author=?
+               WHERE id=?`
+    conn.query(sql, [title,description,author, id], (err,rows,fields) =>{
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.redirect('/topic/'+id);
+        }
+    })
+})
+
 app.get('/topic/add', (req,res) => {
     var sql = `SELECT id,title FROM topic`
     conn.query(sql, (err, topics, fields) => {
@@ -48,11 +86,36 @@ app.post('/topic/add',(req,res) => {
             res.redirect('/topic/'+result.insertId)
         }
     })
-        
-        
-    
 })
 
+app.get('/topic/:id/delete', (req,res) => {
+    var sql = `SELECT id,title FROM topic`
+    var id = req.params.id
+    conn.query(sql, (err, topics, fields) => {
+        var sql =`SELECT * FROM topic WHERE id=?`
+        conn.query(sql,[id], (err, topic) => {
+            if(err){
+                console.log(err);
+                res.status(500).send('Internal Server Error')
+            } else {
+            // 데이터베이스에 값이 없으면 에러 출력
+                if(topic.length === 0){
+                    console.log('There is no record');
+                    res.status(500).send('Internal Server Error')
+                } else {
+                    res.render('delete',{topics:topics, topic:topic[0]})
+                }
+            }
+        })
+    })
+})
+app.post('/topic/:id/delete', (req,res) => {
+    var sql = 'DELETE FROM topic WHERE id=?'
+    var id = req.params.id;
+    conn.query(sql, [id], (err,result) => {
+        res.redirect('/topic/')
+    })
+})
 app.get(['/topic', '/topic/:id'], (req,res) => {
     var sql = `SELECT id,title FROM topic`
     conn.query(sql, (err, topics, fields) => {
@@ -71,45 +134,7 @@ app.get(['/topic', '/topic/:id'], (req,res) => {
         res.render('view', {topics:topics})
         }
     })
-    /*
-    fs.readdir('data', (err,files) => {
-        if(err){
-            console.log(err);
-            res.status(500).send("Internal Server Error")
-        }
-    var id = req.params.id
-    if(id){
-        //id 값이 있을 때 
-        fs.readFile('data/'+id, 'utf8', (err,data) => {
-        if(err) {
-            console.log(err);
-            res.status(500).send("Internal Server Error")
-        }
-        res.render('view',{topics:files, title:id , description : data});
-      })
-    } else {
-        // id 값이 없을 떄
-        res.render('view', {topics:files, title:'Welcome', description:'Hello, Javascript for server.'})
-    }
-    })
-    */
 })
-// app.get('/topic/:id', (req,res) => {
-//     var id = req.params.id;
-//     fs.readdir('data',(err,files) => {
-//         if(err){
-//             console.log(err);
-//             res.status(500).send("Internal Server Error")
-//         }
-//         fs.readFile('data/'+id, 'utf8', (err,data) => {
-//             if(err) {
-//                 console.log(err);
-//                 res.status(500).send("Internal Server Error")
-//             }
-//             res.render('view',{topics:files, title:id , description : data});
-//         })
-//     }) 
-// })
 
 app.get('/' ,(req,res) => {
     res.render('index')
