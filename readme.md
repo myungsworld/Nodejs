402ce076581e66afafcc8e18ea43da238b1664d5
myungsworld <myungsworld@gmail.com>
2020-07-27 10:42:40 +0900

    add implemented

--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
+## body-parser
+
+[Node.js body parsing middleware](https://www.npmjs.com/package/body-parser)  
+
+*  __app.use(bodyParser.urlencoded([options])__   
+
+ A new body object containing the parsed data is populated on the request object after the middleware (i.e. req.body). This object will contain key-value pairs, where the value can be a string or array (when extended is false), or any type (when extended is true).
+
 ## Node MySQL 2
 
 [node-mysql](https://github.com/sidorares/node-mysql2)
--- study/app_mysql_clone.js
+++ study/app_mysql_clone.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const bodyParse = require('body-parser')
+const bodyParser = require('body-parser')
 const port = 3000
 
 var mysql = require('mysql2')
@@ -18,6 +18,53 @@ app.set('view engine', 'pug')
 app.set('views','./views_mysql')
 
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/topic/add', (req,res) => {
+    var sql = `SELECT id,title FROM topic`
+    conn.query(sql, (err, topics, fields) => {
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error')
+        } else {
+            res.render('add',{topics:topics})
+        }
+    })
+})
+
+app.post('/topic/add', (req,res) => {
+    var title = req.body.title
+    var description = req.body.description
+    var author = req.body.author
+    var sql = `INSERT INTO topic (title,description,author) VALUES (?,?,?)`
+    conn.query(sql, [title,description,author], (err, result) => {
+        if(err){
+            console.log(err)
+            res.status(500).send('Internal Server Error')
+        } else {
+            res.redirect('/topic')
+        }
+    })
+})
+
+app.get(['/topic','/topic/:id'], (req,res) => {
+    var sql = `SELECT id,title FROM topic`
+    conn.query(sql, (err, topics, fields) => {
+        var id = req.params.id
+        if(id){
+            var sql =`SELECT * FROM topic WHERE id=?`
+            conn.query(sql, [id], (err, topic, fields) => {
+                if(err){
+                    console.log(err);
+                    res.status(500).send('Internal Server Error')
+                }else{
+                    res.render('view' ,{topics:topics , topic:topic[0]})
+                }
+            })
+        }else{
+            res.render('view', {topics:topics})
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log('local host 3000 is connected')
