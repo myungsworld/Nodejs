f3b716f1406a99c794185f94af6b2cf63d3e8b6a
myungsworld <myungsworld@gmail.com>
2020-08-01 13:19:46 +0900

    updated

--- study/app_session.js
+++ study/app_session.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var session = require('express-session');
+var bkfd2Password = require("pbkdf2-password");
 const bodyParser = require('body-parser');
 const e = require('express');
 var app = express();
+var hasher = bkfd2Password();
 
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -35,29 +37,94 @@ app.get('/welcome', (req,res) => {
     } else {
         res.send(`
         <h1>Welcome</h1>
-        <a href='/auth/login'>Login</a>
+        <ul>
+        <li><a href='/auth/login'>Login</a></li>
+        <li><a href='/auth/register'>Register</a></li>
+        </ul>    
         `)
     }
 
 })
 
-app.post('/auth/login', (req,res) =>{
-    var user = {
+var users = [
+    {
         username:'song',
-        password:'111',
+        password:'qqs8M5dX3+xmZJ3/2+hWnVivSbMvWqIlOruzj5Qn5R9N2S1TRIC0dgJRKA04YT4HBacTB3lhclZCQJ6rIQLWfsMpNCPYJcVYspYDlDCJkG3ciky1hCfnvjTflhbTpaitLymDIHKWU0xApmNEDiM//MYmmzbEQQiCqXeqRUXeVos=',
+        salt:'5E87M9IJjfl+MZsGn9VIj51VGeLmCHT2NDWOf8Gnc+hTRJ98lkEIFjam8tOYibLFiyjcujR8vPTVlkhUceTnLw==',
         displayName:'song'
+    },
+    {
+        username:'egoing',
+        password:'75feb543611a07d828c82d308e81a759e95f5cc51fe5fbfe3a27bbddb51bcdd4',
+        salt:'@$#$njkrsf',
+        displayName:'egoing'
     }
+
+]
+app.post('/auth/register', (req,res) => {
+    return hasher({password:req.body.password},function(err,pass,salt,hash){
+        var user = {
+            username:req.body.username,
+            password:hash,
+            displayName:req.body.displayName
+        }
+        users.push(user);
+        req.session.displayName = req.body.displayName
+        req.session.save(function(){
+            res.redirect('/welcome')
+        })
+    })
+})
+
+app.get('/auth/register', (req,res) => {
+    var output = `
+    <h1>Register</h1>
+    <form action='/auth/register' method ='post'>
+        <p>
+            <input type = 'text' name ='username' placeholder='username'>
+        </p>
+        <p>
+            <input type="password" name="password" placeholder="password">
+        </p>
+        <p>
+            <input type='text' name='displayName' placeholder="displayName">
+        </p>
+        <p>
+            <input type='submit'>
+    </form>     
+    `
+    res.send(output)
+})
+
+
+app.post('/auth/login', (req,res) => {
     var uname = req.body.username
     var pwd = req.body.password
-    if(uname === user.username && pwd === user.password){
-        //로그인 성공하면, 밑 코드 실행, 그 세션 아이디에 displayName
-        //이라는 값으로 user의 displayName 을 저장함
-        //서버에 정보 저장
-        req.session.displayName = user.displayName
-        res.redirect(`/welcome`)
-    } else {
-        res.send(`<a href='/auth/login'>fuck off you motherfucker</a>`)
-    }
+    for(var i=0; i<users.length; i++){
+        var user = users[i]
+        if(uname === user.username) {
+            //콜백함수가 나중에 실행되는걸 방지하기 위함
+            return hasher({password:pwd, salt:user.salt}, (err,pass,salt,hash) => {
+                if(hash === user.password){
+                    req.session.displayName = user.displayName;
+                    req.session.save(function(){
+                        res.redirect('/welcome');
+                    })
+                } else {
+                    res.send(`<a href='/auth/login'>fuck off you motherfucker</a>`)
+                }
+            })
+        }
+    //    if(uname === user.username && sha256(user.salt+pwd) === user.password){
+    //         //로그인 성공하면, 밑 코드 실행, 그 세션 아이디에 displayName
+    //         //이라는 값으로 user의 displayName 을 저장함
+    //         //서버에 정보 저장
+    //          req.session.displayName = user.displayName
+    //         return req.session.save(function(){
+    //             res.redirect(`/welcome`)
+    //         })
+    //     } 
+    }    
 })
 
 app.get('/auth/login', (req,res) => {
